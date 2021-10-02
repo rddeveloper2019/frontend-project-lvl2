cdd34df9362c8f430674f33e72a955842094c3b1
Ridvan Dzhalil <fatzey2010@gmail.com>
2021-10-02 16:06:08 +0300

    third step demo

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,36 @@
 #!/usr/bin/node
 
 import program from 'commander';
+import path from 'path';
+import fs from 'fs';
+import getDifference from '../src/index.js';
 
 const version = '1.0.0';
+
+const getFilePath = (filepath = '') => {
+  const res = path.resolve(process.cwd(), filepath);
+  return res;
+};
+
+const checkAndReadFile = (file) => {
+  if (path.extname(file).length === 0) {
+    return '';
+  }
+  return fs.readFileSync(file, 'utf-8', (err, data) => {
+    if (err) return err;
+    return data;
+  });
+};
 
 program
   .version(version)
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action(() => {
+    const file1 = getFilePath(process.argv[2]);
+    const file2 = getFilePath(process.argv[3]);
+
+    getDifference(checkAndReadFile(file1), checkAndReadFile(file2));
+  });
 
 program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,57 @@
+import _ from 'lodash';
+
+const getResultString = (separatedArray) => {
+  const temp = [];
+  _.cloneDeep(separatedArray).forEach((item) => {
+    if (Array.isArray(item[0])) {
+      item.forEach((el) => temp.push(el));
+      return;
+    }
+    temp.push(item);
+  });
+
+  let resultString = '';
+  _.sortBy(temp, (el) => el[1]).forEach((element) => {
+    resultString += `${element[0]} ${element[1]}: ${element[2]}\n`;
+  });
+
+  return resultString;
+};
+
+const getSeparatedByKey = (firstObj, secondObj, key) => {
+  if (!_.has(firstObj, key) && _.has(secondObj, key)) {
+    return ['+', key, secondObj[key]];
+  }
+
+  if (_.has(firstObj, key) && !_.has(secondObj, key)) {
+    return ['-', key, firstObj[key]];
+  }
+
+  if (_.isEqual(firstObj[key], secondObj[key])) {
+    return [' ', key, firstObj[key]];
+  }
+
+  return [
+    ['-', key, firstObj[key]],
+    ['+', key, secondObj[key]],
+  ];
+};
+
+const getDifference = (first, second) => {
+  const firstObj = !first ? {} : JSON.parse(first);
+  const secondObj = !second ? {} : JSON.parse(second);
+  const allKeys = Object.keys(
+    Object.assign(_.cloneDeep(firstObj), _.cloneDeep(secondObj)),
+  );
+
+  const separated = [];
+  allKeys.forEach((key) => {
+    separated.push(getSeparatedByKey(firstObj, secondObj, key));
+  });
+  const difference = getResultString(separated);
+
+  console.log(difference);
+  return difference;
+};
+
+export default getDifference;
