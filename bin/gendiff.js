1ba18899157dbc8af18301f2237f81566548fd4d
Ridvan Dzhalil <fatzey2010@gmail.com>
2021-10-02 11:19:48 +0300

    -h -f [type] added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,9 @@ import program from 'commander';
 
 const version = '1.0.0';
 
-program.version(version);
-
-program.description('Compares two configuration files and shows a difference.');
+program
+  .version(version)
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
