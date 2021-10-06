332eded21b0cc3b7d7658fc2c8e168330ce9c9dd
Ridvan Dzhalil <fatzey2010@gmail.com>
2021-10-06 14:11:51 +0300

    filtering completed

--- src/stylizers/stylish.js
+++ src/stylizers/stylish.js
@@ -18,7 +18,7 @@ const getMarkerBy = (status) => {
   return markers[status];
 };
 
-const print = (valueWithMeta) => {
+const printSimple = (valueWithMeta) => {
   const {
     key, value, oldValue, nestingLevel: count, newValueStatus, status,
   } = valueWithMeta;
@@ -27,51 +27,48 @@ const print = (valueWithMeta) => {
   let line = '';
 
   if (newValueStatus) {
-    lineBefore += `${addSpaces(count)}${getMarkerBy(status)}${key}: ${oldValue}\n`;
-    line = `${addSpaces(count)}${getMarkerBy(newValueStatus)}${key}: ${value}\n`;
+    lineBefore += `${addSpaces(count)}${getMarkerBy(status)} ${key}: ${oldValue}\n`;
+    line = `${addSpaces(count)}${getMarkerBy(newValueStatus)} ${key}: ${value}\n`;
     return `${lineBefore}${line}`;
   }
 
-  line = `${addSpaces(count)}${getMarkerBy(status)}${key}: ${value}\n`;
+  line = `${addSpaces(count)}${getMarkerBy(status)} ${key}: ${value}\n`;
 
   return line;
 };
 
 const stylish = (valuesWithMeta) => {
-  const linesArray = valuesWithMeta.map((item) => {
-    const {
-      key, nestingLevel: count, children,
-    } = item;
+  const newData = ['{\n', ..._.cloneDeep(valuesWithMeta), '}'];
 
-    const { status } = item;
-    const newStatus = 'NON';
-    const oldStatus = status;
+  const printAll = (values) => {
+    const linesArray = values.map((item) => {
+      if (item === '{\n' || item === '}') {
+        return item;
+      }
 
-    if (item.elementType === 'Simple') {
-      return print(item);
-    }
+      const {
+        key, nestingLevel: count, children, status,
+      } = item;
 
-    let marker = getMarkerBy(newStatus);
+      if (item.elementType === 'Simple') {
+        return printSimple(item);
+      }
 
-    if (oldStatus === 'ADDED' || oldStatus === 'DELETED' || oldStatus === 'NON') {
-      children.map((child) => { child.status = newStatus; });
-      marker = getMarkerBy(oldStatus);
-    }
+      let marker = getMarkerBy('NON');
 
-    const line = `${addSpaces(count)}${marker}${key}: {\n${stylish(children)}${addSpaces(count)}}\n`;
+      if (status === 'ADDED' || status === 'DELETED' || status === 'NON') {
+        children.map((child) => { child.status = 'NON'; });
+        marker = getMarkerBy(status);
+      }
 
-    return line;
-  });
+      const line = `${addSpaces(count)}${marker} ${key}: {\n${printAll(children)}${addSpaces(count)}}\n`;
+      return line;
+    });
 
-  const string = linesArray.join('');
-  // string += '}';
-  // linesArray.forEach((line) => {
-  //   string += line;
-  // });
+    return linesArray.join('');
+  };
 
-  // let result = _;
-
-  return string;
+  return printAll(newData);
 };
 
 export default stylish;
