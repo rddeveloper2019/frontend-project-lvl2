b56f1e0dc226df808320504488856e5e75bf4f15
Ridvan Dzhalil <fatzey2010@gmail.com>
2021-10-09 20:21:19 +0300

    removed let from tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,16 +13,14 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let fileA;
-let fileB;
-let result;
+const fileA = getFixturePath('big1.json');
+const fileB = getFixturePath('big2.yaml');
+
 const blankObj = {};
 
 describe('STYLISH test', () => {
   test('big1.yml big2.json', () => {
-    fileA = getFixturePath('big1.yml');
-    fileB = getFixturePath('big2.json');
-    result = readFile('toEqual/resultStylish');
+    const result = readFile('toEqual/resultStylish');
 
     expect(genDiff(fileA, fileB)).toEqual(result);
   });
@@ -30,9 +28,7 @@ describe('STYLISH test', () => {
 
 describe('PLAIN test', () => {
   test('big1.json big2.yaml', () => {
-    fileA = getFixturePath('big1.json');
-    fileB = getFixturePath('big2.yaml');
-    result = readFile('toEqual/resultPlain');
+    const result = readFile('toEqual/resultPlain');
 
     expect(genDiff(fileA, fileB, 'plain')).toEqual(result);
   });
@@ -40,9 +36,7 @@ describe('PLAIN test', () => {
 
 describe('JSON test', () => {
   test('big1.json big2.yaml', () => {
-    fileA = getFixturePath('big1.json');
-    fileB = getFixturePath('big2.yaml');
-    result = readFile('toEqual/resultJSON');
+    const result = readFile('toEqual/resultJSON');
 
     expect(genDiff(fileA, fileB, 'json')).toEqual(result);
   });
